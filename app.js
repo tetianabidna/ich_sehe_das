9f093594f0666335de89347389e88a9865825a24
Tetiana Bidna <dedivana78@gmail.com>
2020-01-15 00:42:31 +0100

    Erste abgabe
    The process of level building was automated
    Scene was extended
    Data was rearranged

--- app.js
+++ app.js
@@ -1,33 +1,89 @@
 App = function() {
-  this.foundDifferences = 0;
-  this.totalDifferences = 7;
-  
-  this.sceneNumber = 0;
-  var sceneInfo;
+
+  this.totalDifferences;
+  this.allowedClicks;
+  this.isDifference = false;
+  this.completedLevelIDs = [];
+  this.currentLevel = -1;
 
   // this is where the WADE app is initialized
   this.init = function() {
+
     // load a scene
     load();
   };
 
-  this.gameOver = function() {
-    wade.app.sceneNumber += 1;
+  function load() {
+    console.log('LOAD START SCENE');
+
     wade.clearScene();
+    wade.loadScene('scenes/StartScene.wsc', true, function() {
+      loadLevelPreViews(getDataJSON());
+
+      var completedLevelIDs = wade.app.completedLevelIDs;
+      for (i = 0; i < completedLevelIDs.length; i += 1) {
+
+        var obj = wade.getSceneObject('gemacht_' + completedLevelIDs[i]);
+        obj.setVisible(true);
+      }
+    });
+  }
+
+  this.gameOver = function() {
+
+    console.log('GAME OVER');
+
+    wade.clearScene();
+
+    wade.loadScene('scenes/GameOverScene.wsc', true, function() {});
+
+    setTimeout(function() {
+      wade.clearScene();
+      load();
+    }, 2000);
+  }
+
+  this.continueGame = function() {
+
+    console.log('WON THE GAME');
+
+
+    var exist = false;
+
+    for (i = 0; i < wade.app.completedLevelIDs.length; i += 1) {
+      if (wade.app.completedLevelIDs[i] == wade.app.currentLevel) {
+        exist = true;
+        break;
+      }
+
+    }
+
+    if (!exist) {
+      wade.app.completedLevelIDs.push(wade.app.currentLevel);
+    }
+
     load();
   }
 
-  function load() {
-    wade.loadScene('scene1.wsc', true, function() {
-    
-      //wade.app.foundDifferences = 0;
-      
-      //sceneInfo = getNextSceneInfo( wade.app.sceneNumber);
+  this.buildAndLoadNewScene = function(idOfImg) {
+    console.log('BUILD NEW LEVEL');
 
-      //buildNewScene(sceneInfo);
+    wade.clearScene();
 
-      wade.getSceneObject('scoreText').getSprite(0).setText(
-        wade.app.foundDifferences + " / " + wade.app.totalDifferences);
+    wade.app.currentLevel = idOfImg;
+
+    wade.loadScene('scenes/LevelScene.wsc', true, function() {
+
+      var sceneInfo = getSceneInfo(idOfImg);
+      wade.app.totalDifferences = sceneInfo.coordinatesOfDifferences.length;
+      wade.app.allowedClicks = sceneInfo.coordinatesOfDifferences.length - 2;
+
+    console.log('Before');
+      buildNewScene(sceneInfo);
+
+      wade.getSceneObject('scoreText').getSprite(0).setText('Finde ' + wade.app.totalDifferences + ' Unterschiede');
+      wade.getSceneObject('dollarClicks').getSprite(0).setText(wade.app.allowedClicks);
+
     });
   }
 };
